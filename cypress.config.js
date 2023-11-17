6c9eb96eed008a93379749fe97c3ded54f308d64
Naufal Azhar <naufalazhar65@gmail.com>
2023-11-17 17:28:12 +0700

    commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  // defaultCommandTimeout: 4000,
+  // pageLoadTimeout: 30000,
   projectId: 'koxvv5',
   e2e: {
     setupNodeEvents(on, config) {
